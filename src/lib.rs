91e96121006f5d3a163daa2815492820a95c2ef6
abh1nav10 <satsyndicate@gmail.com>
2025-09-27 09:42:11 +0000

    tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-/* Tweak is a data structure which allocates nodes on the heap having a pointer to the next and previous 
+/* Tweak is a data structure which allocates nodes on the heap having a pointer to the next and previous
    and the value itself. Tweak itself has a head which points to the first node. Upon reaching (max - 1),
-   on insertion from the insert_from_head the last node starts pointing to the head thus completing the cycle 
-   and upon further insertion the last node is dropped the new element is inserted which becomes the head if 
-   done from the insert_from_head method and that also has a pointer to the previous node and the previous node 
+   on insertion from the insert_from_head the last node starts pointing to the head thus completing the cycle
+   and upon further insertion the last node is dropped the new element is inserted which becomes the head if
+   done from the insert_from_head method and that also has a pointer to the previous node and the previous node
    has a pointer to the new head thus maintaining the cycle.
 
    * This data structure has been implemented solely for fun
@@ -15,41 +15,43 @@
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
+#[derive(Debug)]
 pub struct Node<T> {
     #[allow(unused)]
-    value : T,
-    prev : Option<NonNull<Node<T>>>,
-    next : Option<NonNull<Node<T>>>,
+    value: T,
+    prev: Option<NonNull<Node<T>>>,
+    next: Option<NonNull<Node<T>>>,
 }
 
+#[derive(Debug)]
 pub struct Tweak<T> {
-    max : usize,
-    length : usize,
-    head : Option<NonNull<Node<T>>>,
-    marker : PhantomData<Node<T>>,
+    max: usize,
+    length: usize,
+    head: Option<NonNull<Node<T>>>,
+    marker: PhantomData<Node<T>>,
 }
 
 impl<T> Node<T> {
-    fn new(value : T) -> Self {
+    fn new(value: T) -> Self {
         Node {
             value,
-            prev : None,
-            next : None,
+            prev: None,
+            next: None,
         }
     }
 }
 
 impl<T> Tweak<T> {
-    pub fn new(max : usize) -> Self {
+    pub fn new(max: usize) -> Self {
         Tweak {
             max,
-            length : 0,
-            head : None,
-            marker : PhantomData,
+            length: 0,
+            head: None,
+            marker: PhantomData,
         }
     }
 
-    pub fn insert_from_head(&mut self, value : T) {
+    pub fn insert_from_head(&mut self, value: T) {
         let mut new = Node::new(value);
         match self.head {
             None => {
@@ -83,22 +85,21 @@ impl<T> Tweak<T> {
                         let iteration = self.max - 2;
                         for _ in 0..iteration {
                             if let Some(a) = copy {
-                                let next = unsafe {
-                                    (*a.as_ptr()).next
-                                };
+                                let next = unsafe { (*a.as_ptr()).next };
                                 copy = next;
                             }
                         }
                         new.prev = copy;
                         let allocated = NonNull::new(Box::into_raw(Box::new(new)));
                         unsafe {
-                            (*t.as_ptr()).prev = allocated;  
+                            (*t.as_ptr()).prev = allocated;
                         }
                         let p = copy;
                         unsafe {
                             (*p.unwrap().as_ptr()).next = allocated;
                         }
                         self.head = allocated;
+                        self.length += 1;
                     }
                     _ => {
                         new.next = self.head;
@@ -107,10 +108,15 @@ impl<T> Tweak<T> {
                             (*t.as_ptr()).prev = allocated;
                         }
                         self.head = allocated;
+                        self.length += 1;
                     }
                 }
             }
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
     }
 }
 
@@ -123,30 +129,41 @@ impl<T> Drop for Tweak<T> {
                 if self.length == self.max {
                     let mut last = self.head;
                     for _ in 0..(self.length - 1) {
-                        last = unsafe {
-                            (*last.unwrap().as_ptr()).next
-                        };
+                        last = unsafe { (*last.unwrap().as_ptr()).next };
                     }
                     unsafe {
                         (*last.unwrap().as_ptr()).next = None;
                     }
                     while let Some(a) = check {
-                        let to_be_dropped = unsafe {
-                            Box::from_raw(a.as_ptr())
-                        };
+                        let to_be_dropped = unsafe { Box::from_raw(a.as_ptr()) };
                         check = to_be_dropped.next;
                         drop(to_be_dropped);
                     }
                 } else {
-                   while let Some(a) = check {
-                       let to_be_dropped = unsafe {
-                           Box::from_raw(a.as_ptr())
-                       };
-                       check = to_be_dropped.next;
-                       drop(to_be_dropped);
-                   }
+                    while let Some(a) = check {
+                        let to_be_dropped = unsafe { Box::from_raw(a.as_ptr()) };
+                        check = to_be_dropped.next;
+                        drop(to_be_dropped);
+                    }
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test() {
+        let mut new = Tweak::new(4);
+        new.insert_from_head(1);
+        new.insert_from_head(2);
+        new.insert_from_head(3);
+        println!("{}", new.len());
+        new.insert_from_head(4);
+        new.insert_from_head(5);
+        println!("{}", new.len());
+        assert_eq!(4, new.len());
     }
 }
